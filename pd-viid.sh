6f03b190645fe6c275e5396825ccc64648a2f852
lanyecheirsh <43258955+lanyecheirsh@users.noreply.github.com>
2020-07-02 20:30:44 +0800

    Update pd-viid.sh

--- pd-viid.sh
+++ pd-viid.sh
@@ -31,3 +31,11 @@ mysql --host=$serverIP --port=$port --user=videoweb --password=suntek  --default
 #初始化视图表
 echo "初始化视图表">>/tmp/tmp.log
 mysql --host=$serverIP --port=$port --user=videoweb --password=suntek  --default-character-set=utf8  -D pd_viid <./pd_viid/pd_viid_dep_view.sql 
+
+
+#creat mysqlfile
+baseDir=`pwd`
+time=$(date +"%Y%m%d%H%M%S")
+file_name=test_insert_${time}.sql
+
+echo "INSERT INTO pd_viid.DB_VERSION (VERSION, REMARK, VERSION_TYPE, UPDATE_DATE) VALUES ('视图库v4.0.0', '视图库-v4.0.0-全量脚本', '6', '2020-06-24 17:21:49');\n">>${file_name}
